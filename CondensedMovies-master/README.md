dacbeb855bcf2601982cfe4d70024db5b5ea794b
Mark Rademaker <mtrademaker@gmail.com>
2024-02-12 15:51:18 +0100

    Data Loader Updates

---
+++ CondensedMovies-master/README.md
@@ -0,0 +1,84 @@
+## CondensedMovies
+
+**** ___N.B: Please use the condensed movies challenge https://github.com/m-bain/CondensedMovies-chall with updated splits since some videos in the original paper are unavailable with missing features ****____
+
+___Contact me directly for the additional dataset queries, details in the challenge repo for feature download.___
+
+
+###############################################
+
+This repository contains the video dataset, implementation and baselines from <strong>Condensed Movies: Story Based Retrieval with Contextual Embeddings</strong>.
+
+[Project page](https://www.robots.ox.ac.uk/~vgg/research/condensed-movies) |
+[arXiv preprint](https://arxiv.org/abs/2005.04208) |
+[Read the paper](https://arxiv.org/pdf/2005.04208.pdf) |
+[Preview the data](https://www.robots.ox.ac.uk/~vgg/research/condensed-movies/#preview)
+
+----
+### CondensedMovies Dataset
+
+![videocaptions](figs/example_captions.png)
+
+
+The dataset consists of 3K+ movies, 30K+ professionally captioned clips, 1K+ video hours, 400K+ facetracks & precomputed features from 6 different modalities.
+
+#### Installation
+
+Requirements:
+- Storage
+    - 20GB for features (required for baseline experiments)
+    - 10GB for facetracks
+    - 250GB for videos
+- Libraries
+    - ffmpeg (for video download)
+    - youtube-dl (for video download)
+    - pandas, numpy
+    - python 3.6+
+
+#### Prepare Data
+
+1. Navigate to directory `cd CondensedMovies/data_prep/`
+2. Edit configuration file `config.json` to download desired subsets of the dataset and their destination.
+3. If downloading the source videos (`src: true`), you can edit `youtube-dl.conf` for desired resolution, subtitles etc.
+Please see [youtube-dl](https://github.com/ytdl-org/youtube-dl) for more info
+4. Run `python download.py`
+
+If you have trouble downloading the source videos or features (due to geographical restrictions or otherwise), please contact me.
+
+### Video-Text Retrieval
+
+#### Baseline (Mixture of Expert Embeddings)
+Edit `data_dir` and `save_dir` in `configs/moe.json` for the experiments.
+1. `python train.py configs/moe.json`
+2. `python test.py --resume $SAVED_EXP_DIR/model_best.pth`
+
+### Visualisation
+
+Run `python visualise_face_tracks.py` with the appropriate arguments to visualise face tracks for a given videoID (requires facetracks and source videos downloaded).
+
+#### TODO:
+- [x] youtube download script
+- [x] missing videos check
+- [x] precomputed features download script
+- [x] facetrack visualisation
+- [x] dataloader
+- [x] video-text retrieval baselines
+- [ ] intra-movie baselines + char module
+- [ ] release fixed_seg features
+
+
+#### FAQ
+
+Why did some of the source videos fail to download?
+>This is most likely due to geographical restrictions on the videos, email me at maxbain@robots.ox.ac.uk and I can help.
+
+The precomputed features are averaged over the temporal dimension, will you release the original features?
+>This is to save space, original features in total are ~1TB, contact me to arrange download of this.
+
+I think clip X is incorrectly identified as being from movie Y, what do do?
+>Please let me know any movie identification mistakes and I'll correct it ASAP.
+
+
+#### Acknowledgements
+
+We would like to thank Samuel Albanie for his help with feature extraction.
---
+++ CondensedMovies-master/data_prep/download.py
@@ -0,0 +1,115 @@
+import json
+import os
+from os.path import join as osj
+import pandas as pd
+import pdb
+
+hosting_address = 'https://thor.robots.ox.ac.uk/~vgg/data/condensed-movies/data'
+
+
+def download_features(data_dir):
+    cmd = 'wget {}/features.zip -P {}; unzip {}/features.zip -d {}'.format(hosting_address, data_dir, data_dir, data_dir)
+    os.system(cmd)
+
+
+def download_facetracks(data_dir):
+    cmd = 'wget {}/facetracks.zip -P {}; unzip {}/facetracks.zip -d {}'.format(hosting_address, data_dir,
+                                                                               data_dir, data_dir)
+    os.system(cmd)
+
+
+def youtube_download(data_dir):
+    id_dir = '../data/metadata/youtube-dl-dump'
+    video_dir = osj(data_dir, 'videos')
+    if not os.path.exists(video_dir):
+        os.makedirs(video_dir)
+    for file in os.listdir(id_dir):
+        upload_year = file.replace('.csv', '')
+        video_dir_year = osj(video_dir, upload_year)
+        if not os.path.exists(video_dir_year):
+            os.makedirs(video_dir_year)
+
+        output_fmt = osj(video_dir_year, '%(id)s.%(ext)s')
+        id_fp = osj(id_dir, file)
+        cmd = 'yt-dlp --config-location --ffmpeg-location /usr/local/bin/ffmpeg youtube-dl.conf -o "{}" -a "{}"'.format(output_fmt, id_fp)
+        os.system(cmd)
+
+    # trim advertisement outro from video.
+    trim = None
+    while trim not in ['y', 'n']:
+        trim = str(input(
+            "\nDo you want to trim the videos (y/n)?\nThis removes the advertisements (unrelated to the film), and only needs to be done once per download."))
+
+        if trim not in ['y', 'n']:
+            print('Please type "y" or "n"')
+
+    if trim == "y":
+        trim_video_outro(video_dir)
+
+    # check for failed downloads: this can be due to...
+    # i) geographical restrictions
+    # ii) Too many requests error
+    check_missing_vids(video_dir)
+
+
+def trim_video_outro(video_dir, video_ext='.mkv'):
+    duration_data = pd.read_csv('../data/metadata/durations.csv').set_index('videoid')
+
+    tmp_fp = osj(video_dir, 'tmp' + video_ext)
+    for root, subdir, files in os.walk(video_dir):
+        for file in files:
+            if file.endswith(video_ext) and file != 'tmp' + video_ext:
+                videoid = file.split(video_ext)[0]
+                if videoid not in duration_data.index:
+                    raise ValueError("Videoid not found, video files should be in format {VIDEOID}.mkv")
+
+                video_fp = osj(root, file)
+                new_duration = duration_data.loc[videoid]['duration']
+
+                # create tmp for untrimmed
+                os.system('cp {} {}'.format(video_fp, tmp_fp))
+                cmd = 'ffmpeg -y -ss 0 -i {} -t {} -c copy {}'.format(tmp_fp, new_duration, video_fp)
+                os.system(cmd)
+                os.remove(tmp_fp)
+
+
+def check_missing_vids(video_dir, video_ext='.mkv'):
+    missing_ids = []
+    clips_data = pd.read_csv('../data/metadata/clips.csv').set_index('videoid')
+    for idx, row in clips_data.iterrows():
+        videoid = row.name
+        upload_year = row['upload_year']
+        video_fp = osj(video_dir, str(int(upload_year)), videoid + video_ext)
+        if not os.path.isfile(video_fp):
+            missing_ids.append(videoid)
+
+    success = (1 - len(missing_ids) / len(clips_data)) * 100
+    print('=======================================================\n%.2f %% of clips downloaded successfully' % success)
+    if success == 100:
+        pass
+    elif success < 100:
+        print(
+            '%d clips failed to download. This is likely due to geographical restrictions.\n-->Contact maxbain@robots.ox.ac.uk if this is an issue.' % len(
+                missing_ids))
+
+    with open('missing_videos.out', 'w') as fid:
+        for mid in missing_ids:
+            fid.write(mid + '\n')
+
+
+def main():
+    config = json.load(open('config.json', 'r'))
+    data_dir = config['data_dir']
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    if config['features']:
+        download_features(data_dir)
+    if config['facetracks']:
+        download_facetracks(data_dir)
+    if config['src']:
+        youtube_download(data_dir)
+
+
+if __name__ == "__main__":
+    main()
